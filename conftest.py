6e56b15a85af024311d6ede2a6f192ad06514dc1
Anastasiya Terletskaya <asuta1000@mail.ru>
2024-08-20 17:24:35 +0500

    HW 3: Added 2 test with fixtures

--- conftest.py
+++ conftest.py
@@ -1,13 +1,21 @@
 import pytest
+from selene import browser, be, have
 
 @pytest.fixture(scope="session")
-def browser():
-    print('Browser!')
+def browser_size():
+    browser.config.window_height = 1080  # задает высоту окна браузера
+    browser.config.window_width = 1920  # задает ширину окна браузера
     yield
-    print("Close browser!")
+    browser.quit()
+
+@pytest.fixture(scope="session")
+def browser_example():
+    browser.open('https://google.com')
+    yield
+
 
 @pytest.fixture
-def login_page(browser):
+def login_page(browser_example):
     print('Loging passed!')
     pass
 
---
+++ test_search.py
@@ -0,0 +1,14 @@
+from selene import browser, be, have
+import pytest
+
+
+
+def test_search_1(browser_size):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_search_2(browser_size):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('chmegamogl').press_enter()
+    browser.element('[id="botstuff"]').should(have.text('По запросу chmegamogl ничего не найдено. '))
